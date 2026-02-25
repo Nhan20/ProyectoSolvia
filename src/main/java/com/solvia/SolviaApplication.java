6d23ac1805dcb2347724e1f72f190426386cd6d8
Nhan20 <anniejoanp20@gmail.com>
2026-02-25 09:46:28 -0500

    Se agrega el proyecto

---
+++ src/main/java/com/solvia/SolviaApplication.java
@@ -0,0 +1,13 @@
+package com.solvia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolviaApplication.class, args);
+	}
+
+}
